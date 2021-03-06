c80e58d21ea471f9a7a0dfd80bc6020e43751458
Roman Kudryavtsev <TTe4kiH98@gmail.com>
2021-03-06 13:17:25 +0300

    App edited

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        double regularBonus = 0.31;
+        double specialBonus = 0.62;
+        double totalBonus = regularBonus + specialBonus;
+        System.out.println(totalBonus);
+    }
+}
